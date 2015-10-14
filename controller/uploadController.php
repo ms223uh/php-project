3de167089248044cb1a71e4a8a3e68c7acba183c
Mathias Sundin <ms223uh@student.lnu.se>
2015-10-14 15:36:08 +0200

    Push from new IDE
    2

--- controller/uploadController.php
+++ controller/uploadController.php
@@ -4,6 +4,20 @@ class uploadController {
 
         public function __construct(uploadView $uploadView, uploadModel $uploadModel){
             
+                $this->uploadView = $uploadView;
+                $this->uploadModel = $uploadModel;
+                $this-init();
+
+        }
+
+        public function init(){
+        
+                if($this->uploadView->userWannaUpload()){
+                
+                        
+
+                }
+
         }
 
 }
