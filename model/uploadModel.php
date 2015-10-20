2db0ace1a1c5f9e38e0a71d18d288946eda5df2a
Mathias Sundin <ms223uh@student.lnu.se>
2015-10-20 17:28:51 +0200

    Added some small rules
    Added some small rules. Have to hurry up before deadline.

--- model/uploadModel.php
+++ model/uploadModel.php
@@ -2,6 +2,32 @@
 
 class uploadModel {
     
+    public function rules(){
+        
+            $uploadURL = "/uImages/";
+
+        if(
+            ($_FILES["files"]["type"] == "image/gif")
+            ||($_FILES["files"]["type"] == "image/jpeg")
+            ||($_FILES["files"]["type"] == "image/png")
+          ){
+                   
+                        if($FILES["file"]["size"] < 100000){
+                            return false;
+                    }
+  
+                    else {
+                            if(move_uploaded_file($_FILE["file"]["tmpFile"],
+                                $uploadURL . $_FILES["file"]["name"])){
+                                    return true;
+                                }
+                         }
+                }
+                
+        
+    }
     
+
+
     
 }
